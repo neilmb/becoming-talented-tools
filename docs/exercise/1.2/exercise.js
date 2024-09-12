0eae3145be9c79309d75e3935dc4fabdd8651ecb
Neil Martinsen-Burrell <neili.martinsen-burrell@gsa.gov>
2024-09-12 10:08:09 -0500

    WIP

--- docs/exercise/1.2/exercise.js
+++ docs/exercise/1.2/exercise.js
@@ -1,12 +1,30 @@
 class IntervalCycles extends HTMLElement {
 
+  constructor() {
+    super();
+    this.interval = "2nds";
+    this.direction = "Ascending";
+    this.starting_note = "Do";
+
+  static get observedAttributes() {
+      return ["interval", "direction", "starting_note"];
+  }
+
+  attributeChangedCallback(property, oldValue, newValue) {
+      if (oldValue === newValue) return;
+      this[ property ] = newValue;
+      render();
+  }
+
   // connect component
   connectedCallback() {
     this.attachShadow({ mode: 'open' });
+    render();
+  }
 
-    const template = document.getElementById("exercise-template");
-
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  render() {
+    template_content = `<p>${this.direction} ${this.interval} starting from ${this.starting_note}</p>`
+    this.shadowRoot.innerHTML = template_content;
   }
 }
 
